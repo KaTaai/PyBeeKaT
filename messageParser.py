a64b0ba33a26479da6cf29133e2dc0950f786996
KaTaai <powerlinch@hotmail.com>
2017-04-16 19:21:00 +0200

    Added comments and small regex tweaks

--- messageParser.py
+++ messageParser.py
@@ -1,9 +1,13 @@
 import re
 
 def parse(message):
+    """
+    Parses 3 types of messages that the bot received
+    """
     messageRegex = re.compile(r"""(PING)\s:(\d+)|  #the Ping message
-:([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)!\~([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)@[\w\.]+\s([A-Z]+)\s([#@]?[a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s:?(.*)| #evaluates f-ex the MODE message
-:[\w\.]+\s(\d{3})\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)\s(.*)""", re.VERBOSE)
+:([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)!\~?([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)@[\w\.-]+\s([A-Z]+)\s([#@]?[a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s?:?(.*)| #evaluates f-ex the MODE message
+:[\w\.]+\s(\d{3})\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)\s(.*)    # Parses the messages including the /MOTD
+""", re.VERBOSE)
     messageList = messageRegex.findall(message)
     if len(messageList) == 0:
         return messageList
