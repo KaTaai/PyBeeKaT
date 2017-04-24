29c1f543c780e772445a06c13c9f97a80fc41e64
KaTaai <powerlinch@hotmail.com>
2017-04-25 00:21:49 +0200

    changed the main loop in Botbase.py and changed the regexes in messageParser.py

--- Botbase.py
+++ Botbase.py
@@ -31,24 +31,46 @@ class Bot(object):
         logging.info("Bot is connected")
         
         
+    def message(self,message):
+        self.irc.send(message.encode())
+        
     def userMessage(self, username, hostname, servername, realname):
-        self.irc.send(" ".join(["USER", username, hostname, servername, realname,"\n"]).encode())
+        self.message(" ".join(["USER", username, hostname, servername, realname,"\n"]))
         
     def nickMessage(self, username):
-        self.irc.send(" ".join(["NICK", username, "\n"]).encode())
+        self.message(" ".join(["NICK", username, "\n"]))
         
     def joinMessage(self, channel):
-        self.irc.send(" ".join(["JOIN", channel, "\n"]).encode())
+        self.message(" ".join(["JOIN", channel, "\n"]))
         logging.info("Joining :"+ channel)
         
     def pingMessage(self,response):
-        self.irc.send(" ".join(["PONG", response, "\n"]).encode())
+        self.message(" ".join(["PONG", response, "\n"]))
         logging.debug("PONG :"+ response)
         
+    def partMessage(self, channel):
+        self.message(" ".join(["PART", channel,"\n"]))
+        logging.info("leaving the channel: "+ channel)
+        
     def quitMessage(self,message=""):
-        self.irc.send(" ".join(["QUIT", message,"\n"]).encode())
+        self.message(" ".join(["QUIT :", message,"\n"]))
         logging.info("quitting and shutting down: "+ message)
         self.isConnected = False
+        
+    def whoisMessage(self,nick):
+        self.message(" ".join(["WHOIS", nick, "\n"]))
+        logging.info("Whois query sent about: " + nick)
+        
+    def PRIVMSGMessage(self,receiver,message):
+        
+        self.message(" ".join([
+            ":"+self.settings.botnick,
+            "PRIVMSG",
+            receiver,
+            ":"+message,
+            "\n"
+            ]))
+        logging.info("Message: '" + message + "' sent to: '" + receiver + "'")
     
 B=Bot()
 
@@ -58,7 +80,6 @@ while B.isConnected:
     text=B.irc.recv(2040)
     textBuffer = textBuffer + text.decode()
     textBufferList = textBuffer.split("\n")
-    logging.debug("End of the textBufferList: "+ textBufferList[-1])
     if textBufferList[-1].endswith("\r"):
         a=1
         textBuffer = ""
@@ -68,25 +89,50 @@ while B.isConnected:
     i = 0
     while i <= len(textBufferList)-2+a:
         message = messageParser.parse(message=textBufferList[i].strip())
-        logging.debug(message)
-        logging.debug(textBufferList[i].strip())
+        #logging.debug(message)
+        #logging.debug(textBufferList[i].strip())
         if len(message)==0:
-            pass
+            logging.debug("message that isn't parsed: "+ textBufferList[i].strip())
         elif message[0] == "PING":
             B.pingMessage(message[1])
-        elif 'End of /MOTD command.' in message[2]:
-            B.joinMessage(B.settings.channel)
-        elif message[2] == 'PRIVMSG':
-            if message[4][0] == B.settings.commandCharacter:
-                commandsep = message[4][1:].strip().split(" ",1)
-                if commandsep[0].lower()=="join":
-                    if len(commandsep) == 2:
-                        B.joinMessage(commandsep[1])
-                elif commandsep[0].lower() == "quit":
-                    if len(commandsep) == 1:
-                        B.quitMessage()
-                    else:
-                        B.quitMessage(commandsep[1])
+            
+        else:
+            try:
+                code = int(message[0])
+                if code == 376:
+                    B.joinMessage(B.settings.channel)
+                elif code == 372:
+                    pass
+                else:
+                    logging.debug("codemessage that is parsed: "+ textBufferList[i].strip())
+            except ValueError:
+                if message[1] == 'PRIVMSG':
+                    logging.debug("PRIVMSG that is parsed: "+ textBufferList[i].strip())
+                    if message[3][0] == B.settings.commandCharacter:
+                        commandsep = message[3][1:].strip().split(" ",1)
+                        if commandsep[0].lower()=="join":
+                            if len(commandsep) == 2:
+                                B.joinMessage(commandsep[1])
+                        elif commandsep[0].lower() == "quit":
+                            if len(commandsep) == 1:
+                                B.quitMessage()
+                            else:
+                                B.quitMessage(commandsep[1])
+                        elif commandsep[0].lower() == "part":
+                            if len(commandsep) == 1:
+                                B.partMessage(message[3])
+                            else:
+                                B.partMessage(commandsep[1])
+                        elif commandsep[0].lower() == "whois":
+                            if len(commandsep)> 1:
+                                B.whoisMessage(commandsep[1])
+                        elif commandsep[0].lower() == "say":
+                            if len(commandsep)> 1:
+                                B.PRIVMSGMessage(message[2],commandsep[1])
+                elif message[1] == "NOTICE":
+                    logging.info("Notice from: '" + message[0] + "' on: '" + message[2] + "' with: '" + message[3] + "'")
+                else:
+                    logging.debug("message that is parsed: "+ textBufferList[i].strip())
     
         i+=1
         
--- messageParser.py
+++ messageParser.py
@@ -4,10 +4,15 @@ def parse(message):
     """
     Parses 3 types of messages that the bot received
     """
-    messageRegex = re.compile(r"""(PING)\s:(\d+)|  #the Ping message
-:([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)!\~?([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)@[\w\.-]+\s([A-Z]+)\s([#@]?[a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s?:?(.*)| #evaluates f-ex the MODE message
-:[\w\.]+\s(\d{3})\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)\s(.*)    # Parses the messages including the /MOTD
-""", re.VERBOSE)
+    messageRegex = re.compile(r"""
+        (PING)\s:(.*)|  #the Ping message
+        :([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)![a-zA-Z_\\\[\]\{\}\^`|\d\~-]+@[\w\.-]+\s([A-Z]+)\s([#@]?[a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s?:?(.*)| #evaluates f-ex the MODE message
+    :[\w\.]+\s(\d{3})\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|]+)\s(.*)|    # Parses the messages including the /MOTD
+    ([A-Z]+)\s:([\w\s]+):\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s.*\s([\w\.]+)\s(.*)| #rudimentary parse of error messages
+    :([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)!~?.*@[\w\.]+\s([A-Z]+)\s:(.*)|
+    ([A-Z]+\s[A-Z]+)\s:[\*]{3}\s(.*)|  #NOTICE AUTH messages
+    :[\w\.]+\s([A-Z]+)\s([a-zA-Z_\\\[\]\{\}\^`|][a-zA-Z_\\\[\]\{\}\^`|\d-]+)\s:(.*)
+    """, re.VERBOSE)
     messageList = messageRegex.findall(message)
     if len(messageList) == 0:
         return messageList
